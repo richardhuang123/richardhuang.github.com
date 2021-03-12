df4d5cf336469b63239fdc37ff76d1e976a957eb
Richard Huang <plyhuang@ucdavis.edu>
2021-03-11 17:52:37 -0800

    des 157

--- DES157/dd/script.js
+++ DES157/dd/script.js
@@ -1,5 +1,5 @@
 (function() {
-alert("Hello! Welcome! First find the picture you want in this loop. Then put the mouse on the picture you have decided." );
+alert("Hello! Welcome! First find the picture you want in this loop. Then put the mouse on the picture you have decided. Finally click on the picture you selected." );
 
 $('.first').hover(function () {
     
