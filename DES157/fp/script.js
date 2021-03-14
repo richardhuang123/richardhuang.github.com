1999bb1daf73b9854086dac0171fd55a18ecbe0a
Richard Huang <plyhuang@ucdavis.edu>
2021-03-13 16:56:26 -0800

    des 157

---
+++ DES157/fp/script.js
@@ -0,0 +1,12 @@
+// (function() {
+// alert("Hello! Welcome! First find the picture you want in this loop. Then put the mouse on the picture you have decided. Finally click on the picture you selected." );
+
+// $('.first').hover(function () {
+    
+//     $('.first').css('animationPlayState', 'paused');
+// },
+
+// function () {
+//     $('.first').css('animationPlayState', 'paused');
+// });
+// }());
